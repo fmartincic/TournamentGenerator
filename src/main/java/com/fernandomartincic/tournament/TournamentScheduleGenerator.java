cd69196e2e32931cac8fc11dd761aed09b04c149
Fernando Martincic <fernando.martincic@gmail.com>
2016-08-24 19:31:41 -0700

    using globally optimal pool selection

--- src/main/java/com/fernandomartincic/tournament/TournamentScheduleGenerator.java
+++ src/main/java/com/fernandomartincic/tournament/TournamentScheduleGenerator.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.apache.commons.io.output.StringBuilderWriter;
-import org.apache.commons.lang3.Validate;
 
 import java.util.*;
 
@@ -21,7 +20,6 @@ public class TournamentScheduleGenerator {
         gen.generate(16, 16, 8);
     }
 
-
     private int numMale = 0;
     private int numFemale = 0;
     private int numPlayers = 0;
@@ -30,6 +28,70 @@ public class TournamentScheduleGenerator {
     private Set<Integer> availableFemale = Sets.newHashSet();
 
     private int[][] matched = null;
+
+    /**
+     * Generates the schedule.
+     *
+     * @param numMale   number of men in the tournament
+     * @param numFemale number of women in the tournament
+     * @param numRounds number of rounds to be played
+     */
+    public void generate(final int numMale, final int numFemale, final int numRounds) {
+        this.numMale = numMale;
+        this.numFemale = numFemale;
+        this.numPlayers = numMale + numFemale;
+        this.numRounds = numRounds;
+
+        resetMatched();
+
+        // generate for each round
+        printMatched();
+
+        for (int round = 0; round < numRounds; round++) {
+            resetAvailable();
+
+            schedule.put(round, Lists.newArrayList());
+
+            System.out.println("\nRound " + (round + 1) + ":");
+
+
+            for (int team = 0; team < numPlayers / 4; team++) {
+
+                final List<Integer> m = new ArrayList<>(availableMale);
+                final List<Integer> f = new ArrayList<>(availableFemale);
+
+                //System.out.println("males=" + availableMale);
+                //System.out.println("females=" + availableFemale);
+
+                final List<List<Integer>> pools = combinePairs(generateAllPairs(m), generateAllPairs(f));
+                //System.out.println("pools=" + pools);
+
+                final List<Integer> pool =
+                        pools.stream()
+                                .min((p1, p2) -> Integer.compare(calculatePoolScore(p1), calculatePoolScore(p2)))
+                                .get();
+                System.out.println("pool=" + pool);
+
+                updateMatched(pool, 1);
+                updateMatched(pool.subList(0, 2), numPlayers * 2);
+                updateMatched(pool.subList(2, 4), numPlayers * 2);
+
+                // remove available players
+                availableMale.removeAll(pool);
+                availableFemale.removeAll(pool);
+
+                schedule.get(round).add(pool);
+
+            }
+
+            System.out.println();
+            printMatched();
+
+        }
+
+//        printSchedule();
+    }
+
 
     private void printMatched() {
         final StringBuilder sb = new StringBuilder();
@@ -80,99 +142,6 @@ public class TournamentScheduleGenerator {
         }
     }
 
-    /**
-     * Generates the schedule.
-     *
-     * @param numMale   number of men in the tournament
-     * @param numFemale number of women in the tournament
-     * @param numRounds number of rounds to be played
-     */
-    public void generate(final int numMale, final int numFemale, final int numRounds) {
-        this.numMale = numMale;
-        this.numFemale = numFemale;
-        this.numPlayers = numMale + numFemale;
-        this.numRounds = numRounds;
-
-        resetMatched();
-
-        // generate for each round
-
-        printMatched();
-
-        for (int round = 0; round < numRounds; round++) {
-            resetAvailable();
-            schedule.put(round, Lists.newArrayList());
-
-            for (int team = 0; team < numPlayers / 4; team++) {
-                System.out.println("\nRound " + (round + 1) + ":");
-                //System.out.println();
-                // get next available players
-                final List<Integer> pool = Lists.newArrayList();
-
-                // get next available male player
-                int i = availableMale.stream().min(Integer::min).get().intValue();
-
-                pool.add(i);
-                System.out.println("male 1 = " + i);
-                availableMale.remove(i);
-                System.out.println(availableMale + " / " + availableFemale);
-
-                // get partner
-                i = availableFemale.stream()
-                        //.peek(j -> System.out.println("Examining j=" + j))
-                        .peek(j -> Validate.isTrue(!pool.contains(j)))
-                        .min((p1, p2) -> {
-                            //System.out.println("p1=" + p1 + ", p2=" + p2);
-                            return Integer.compare(getMatchedScore(pool, p1.intValue()), getMatchedScore(pool, p2.intValue()));
-                        })
-                        .get();
-                System.out.println("female 1 = " + i);
-                pool.add(i);
-                availableFemale.remove(i);
-                //System.out.println(availableMale + " / " + availableFemale);
-
-                // get male opponent
-                i = availableMale.stream()
-                        //.peek(j -> System.out.println("Examining j=" + j))
-                        .peek(j -> Validate.isTrue(!pool.contains(j)))
-                        .min((p1, p2) -> {
-                            //System.out.println("p1=" + p1 + ", p2=" + p2);
-                            return Integer.compare(getMatchedScore(pool, p1.intValue()), getMatchedScore(pool, p2.intValue()));
-                        })
-                        .get();
-                System.out.println("male 2 = " + i);
-                pool.add(i);
-                availableMale.remove(i);
-                //System.out.println(availableMale + " / " + availableFemale);
-
-                // get female opponent
-                i = availableFemale.stream()
-                        //.peek(j -> System.out.println("Examining j=" + j))
-                        .peek(j -> Validate.isTrue(!pool.contains(j)))
-                        .min((p1, p2) -> {
-                            //System.out.println("p1=" + p1 + ", p2=" + p2);
-                            return Integer.compare(getMatchedScore(pool, p1.intValue()), getMatchedScore(pool, p2.intValue()));
-                        })
-                        .get();
-                System.out.println("female 2 = " + i);
-                pool.add(i);
-                availableFemale.remove(i);
-                //System.out.println(availableMale + " / " + availableFemale);
-                updateMatched(pool, 1);
-                updateMatched(pool.subList(0, 2), numPlayers * 2);
-                updateMatched(pool.subList(2, 4), numPlayers * 2);
-
-                //System.out.println(pool);
-                printMatched();
-
-                schedule.get(round).add(pool);
-            }
-        }
-
-
-        //printSchedule();
-    }
-
 
     private void updateMatched(final List<Integer> pool, final int incrementAmount) {
         //System.out.println("pool=" + pool);
@@ -188,28 +157,6 @@ public class TournamentScheduleGenerator {
                                 matched[j][i] += incrementAmount;
                             });
                 });
-    }
-
-    private int getMatchedScore(final List<Integer> pool, final int p2) {
-        //System.out.println("pool=" + pool + ", p2=" + p2);
-        final List<Integer> copy = Lists.newArrayList(pool);
-        copy.add(p2);
-        final List<Integer> copy2 = Lists.newArrayList(copy);
-        Validate.isTrue(copy.containsAll(pool));
-        int total = copy.stream()
-                .map(Integer::intValue)
-                //.peek(i -> System.out.println("i=" + i))
-                .mapToInt(i -> copy2.stream()
-                        .map(Integer::intValue)
-                        .filter(j -> i < j)
-                        //.peek(j -> System.out.println("i=" + i + ", j=" + j))
-                        .mapToInt(j -> matched[i][j])
-                        .sum()
-                )
-                .sum();
-
-        //System.out.println("total=" + total);
-        return total;
     }
 
     private void printSchedule() {
@@ -284,9 +231,27 @@ public class TournamentScheduleGenerator {
 //        for (int t = 0; t < numPlayers; t++) {
 //            System.out.println(totalSumString.get(t));
 //        }
+    }
 
+    final int calculatePoolScore(final List<Integer> pool) {
+        final List<Integer> copy = new ArrayList<>(pool);
+
+        final int total = pool.stream()
+                .mapToInt(Integer::intValue)
+                .map(i -> {
+                    return copy.stream()
+                            .mapToInt(Integer::intValue)
+                            .filter(j -> j < i)
+                            .map(j -> matched[i][j])
+                            .sum();
+                })
+                .sum();
+
+        //System.out.println(pool + "=" + total);
+        return total;
 
     }
+
 
     private String getPlayerTotalScore(final List<String> totals) {
         final StringBuilderWriter sb = new StringBuilderWriter();
@@ -308,5 +273,44 @@ public class TournamentScheduleGenerator {
         }
     }
 
+    private List<List<Integer>> combinePairs(final List<List<Integer>> malePairs, final List<List<Integer>> femalePairs) {
+        final List<List<Integer>> results = new ArrayList<>();
+
+        malePairs.stream()
+                .forEach(malePair -> {
+                    femalePairs.stream()
+                            .forEach(femalePair -> {
+                                final List<Integer> pool = new ArrayList<>();
+
+                                pool.add(malePair.get(0));
+                                pool.add(femalePair.get(0));
+                                pool.add(malePair.get(1));
+                                pool.add(femalePair.get(1));
+
+                                results.add(pool);
+                            });
+                });
+
+        return results;
+    }
+
+    final List<List<Integer>> generateAllPairs(final List<Integer> pool) {
+        final List<List<Integer>> results = new ArrayList<>();
+
+        final List<Integer> poolCopy = new ArrayList<>(pool);
+        pool.stream()
+                .forEach(i -> {
+                    poolCopy.stream()
+                            .filter(j -> i < j)
+                            .forEach(j -> {
+                                List<Integer> pair = new ArrayList<>();
+                                pair.add(i);
+                                pair.add(j);
+                                results.add(pair);
+                            });
+                });
+
+        return results;
+    }
 
 }
